a4df16bdc122cdd17476ea36ec2b2f63dc6e8e46
Harrison D Cole <harrison.d.cole@gmail.com>
2018-11-15 13:12:00 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(n)
-  puts 'Hi, my name is #{n}'
+  puts ("Hi, my name is #{n}.")
 end
 
 def introduction_with_language(n, l)
-  puts 'Hi, my name is #{n} and I am learning to study #{l}'
+  puts ("Hi, my name is #{n} and I am learning to program in #{l}.")
 end
